60e58b5763ed14343667607a5201b5eedd878e12
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-04-14 15:27:35 +0300

    test: fix test tmp dir location for ci

--- tests/helpers/generators/gdal.ts
+++ tests/helpers/generators/gdal.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { mkdtempSync } from 'node:fs';
+import { tmpdir } from 'node:os';
 import { join } from 'node:path';
 import { faker } from '@faker-js/faker';
 import { drivers, SpatialReference, type StringOptions } from 'gdal-async';
 import type { z } from 'zod';
+import { SERVICE_NAME } from '@src/common/constants';
 import type { blockSizeSchema, compressionSchema, layoutSchema, overviewsCountSchema } from '@src/common/schemas';
 import type { InfoGeoTiff } from '../interfaces';
+
+const dirPath = mkdtempSync(join(tmpdir(), `${SERVICE_NAME}-`));
 
 interface GDALCreationOptions {
   driverName: string;
@@ -54,7 +58,7 @@ export const createGDALGeotiffCOGRaster = async (options: CreateGDALRasterOption
   } = options;
 
   const driverGeoTiff = drivers.get('MEM');
-  const filePath = join(__dirname, '../../integration/tmp', faker.system.commonFileName('tif'));
+  const filePath = join(dirPath, faker.system.commonFileName('tif'));
 
   const dataset = await driverGeoTiff.createAsync('', xSize, ySize, BAND_COUNT, dataType, creationOptions);
 
